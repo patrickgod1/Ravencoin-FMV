ab4dbb07c0f9157e10633cb5ec8db54f60c69967
Patrick Yu <patrickgod1@aol.com>
2018-12-22 20:11:06 -0800

    2nd pull from GitHub

--- RavencoinFV.py
+++ RavencoinFV.py
@@ -5,4 +5,4 @@ today = datetime.datetime.today().strftime('%Y%m%d')
 url = f"https://coinmarketcap.com/currencies/ravencoin/historical-data/?start=20130428&end={today}"
 
 df = pd.read_html(url)
-print(df)
+# print(df)
