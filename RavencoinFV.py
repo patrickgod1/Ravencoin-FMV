90466102da3f73dd2d4e786754072dbe82dd4615
Patrick Yu <patrickgod1@aol.com>
2018-12-22 20:42:55 -0800

    read in closing price USD

--- RavencoinFV.py
+++ RavencoinFV.py
@@ -4,5 +4,6 @@ import datetime
 today = datetime.datetime.today().strftime('%Y%m%d')
 url = f"https://coinmarketcap.com/currencies/ravencoin/historical-data/?start=20130428&end={today}"
 
-df = pd.read_html(url)
-# print(df)
+df = pd.read_html(url, header=0, index_col=0)[0]
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+    print(df['Close**'])
